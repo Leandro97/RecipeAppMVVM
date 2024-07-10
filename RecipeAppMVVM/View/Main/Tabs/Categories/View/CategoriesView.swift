c1146e8036ed7b78e9b6bb3bef3798c218d6ae4e
Leandro97 <leandro97.martins@gmail.com>
2024-07-10 10:45:04 -0300

    Changes on Category screen structure

--- RecipeAppMVVM/View/Main/Tabs/Categories/View/CategoriesView.swift
+++ RecipeAppMVVM/View/Main/Tabs/Categories/View/CategoriesView.swift
@@ -8,31 +8,75 @@
 import SwiftUI
 
 struct CategoriesView {
-    @State private var selectedTab: CategoriesTabOptions = .dishType
+    private let dishTypeList: [DishType] = [
+        .breakfast, .appetizer, .salad, .lunch,
+        .dessert, .snack, .dinner, .beverage
+    ]
+    private let dietList: [Diet] = [
+        .vegetarian, .vegan, .dairyFree, .glutenFree,
+        .lactoOvoVegetarian, .pescatarian, .paleolithic, .ketogenic
+    ]
+    
+    @State private var showDishTypeList = false
+    @State private var showDietList = false
+    @State private var selectedCategory: CategoriesTabOptions = .dishType
+    @State private var selectedDishType: DishType = .breakfast
+    @State private var selectedDiet: Diet = .vegetarian
 }
 
 extension CategoriesView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    Picker("", selection: $selectedTab) {
-                        ForEach(CategoriesTabOptions.allCases) { option in
-                            Text(option.title)
+            ZStack {
+                VStack {
+                    List {
+                        Picker("", selection: $selectedCategory) {
+                            ForEach(CategoriesTabOptions.allCases) { option in
+                                Text(option.title)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        if selectedCategory.rawValue == 0 {
+                            VStack(alignment: .leading) {
+                                ForEach(dishTypeList) { dishType in
+                                    Text(dishType.categoryTitle)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .onTapGesture {
+                                            self.selectedDishType = dishType
+                                            self.showDishTypeList = true
+                                        }
+                                }
+                            }
+                        } else {
+                            VStack(alignment: .leading) {
+                                ForEach(dietList) { diet in
+                                    Text(diet.rawValue.capitalized)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .onTapGesture {
+                                            self.selectedDiet = diet
+                                            self.showDietList = true
+                                        }
+                                }
+                            }
                         }
                     }
-                    .pickerStyle(SegmentedPickerStyle())
-                    
-                    if selectedTab.rawValue == 0 {
-                        DishTypeTabView()
-                    } else {
-                        DietTabView()
-                    }
                 }
+                
+                NavigationLink(
+                    destination: CategoriesListView(dishType: selectedDishType),
+                    isActive: $showDishTypeList
+                ) {}
+                
+                NavigationLink(
+                    destination: CategoriesListView(diet: selectedDiet),
+                    isActive: $showDietList
+                ) {}
             }
-            .navigationTitle("Categories")
         }
-        .navigationViewStyle(.stack)
+        .navigationTitle("Categories")
     }
 }
 
